ae67f7dac263380cfc18ceac694ea860aa72f93c
Chrissy Kongshaug <chrissy.singer@gmail.com>
2026-06-09 12:35:53 -0400

    cheking in subqueries

--- Week2/SQL_subqueries.sql
+++ Week2/SQL_subqueries.sql
@@ -7,24 +7,79 @@ USE coffeeshop_db;
 -- Q1) Scalar subquery (AVG benchmark):
 --     List products priced above the overall average product price.
 --     Return product_id, name, price.
-
+select product_id, name, price
+from products
+where price > (
+select avg(price)
+from products
+);
 -- Q2) Scalar subquery (MAX within category):
 --     Find the most expensive product(s) in the 'Beans' category.
 --     (Return all ties if more than one product shares the max price.)
 --     Return product_id, name, price.
-
+select product_id, name, price
+from products
+where category_id = (
+select category_id
+from categories
+where name = 'beans'
+)
+and price = (select max(price)
+from products
+where category_id = (
+select category_id
+from categories 
+where name = 'beans'
+)
+);
 -- Q3) List subquery (IN with nested lookup):
 --     List customers who have purchased at least one product in the 'Merch' category.
 --     Return customer_id, first_name, last_name.
 --     Hint: Use a subquery to find the category_id for 'Merch', then a subquery to find product_ids.
-
+select distinct customer_id, first_name, last_name
+from customers
+where customer_id in (
+select o.customer_id
+from orders o
+where o.order_id in (
+select oi.order_id
+from order_items oi
+where oi.product_id in (
+select p.product_id
+from products p
+where p.category_id = (
+select c.category_id
+from categories c
+where c.name = 'merch'
+)
+)
+)
+);
 -- Q4) List subquery (NOT IN / anti-join logic):
 --     List products that have never been ordered (their product_id never appears in order_items).
 --     Return product_id, name, price.
-
+select product_id, name, price
+from products
+where product_id not in (select product_id from order_items);
 -- Q5) Table subquery (derived table + compare to overall average):
 --     Build a derived table that computes total_units_sold per product
 --     (SUM(order_items.quantity) grouped by product_id).
 --     Then return only products whose total_units_sold is greater than the
 --     average total_units_sold across all products.
 --     Return product_id, product_name, total_units_sold.
+select sub.product_id, p.name as product_name, sub.total_units_sold
+from (
+select product_id, sum(quantity) as total_units_sold
+from order_items
+group by product_id
+) sub
+join products p
+on p.product_id = sub.product_id
+where sub.total_units_sold > (
+select avg(total_units_sold)
+from (
+select product_id, sum(quantity) as total_units_sold
+from order_items
+group by product_id
+) av
+);
